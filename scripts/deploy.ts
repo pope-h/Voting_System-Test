caad36def5aa1eb10969d57198b3449cef416ebe
Ekarika Nsemeke <hamatthan@gmail.com>
2024-02-26 18:42:47 +0100

    added interaction

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -2,12 +2,13 @@ import { ethers } from "hardhat";
 
 async function main() {
   const votingSystemFactory = await ethers.deployContract("VotingSystemFactory");
-
   await votingSystemFactory.waitForDeployment();
 
-  console.log(
-    `Voting System Factory Contract deployed to ${votingSystemFactory.target}`
-  );
+  const votingSystem = await ethers.deployContract("VotingSystem");
+  await votingSystem.waitForDeployment();
+
+  console.log(`Voting System Factory Contract deployed to ${votingSystemFactory.target}`);
+  console.log(`Voting System Contract deployed to ${votingSystem.target}`);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
---
+++ scripts/votingSystemInteraction.ts
@@ -0,0 +1,14 @@
+import { ethers } from "hardhat";
+
+async function main() {
+
+  const votingSystemContractAddress = "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512";
+  const VotingSystem = await ethers.getContractAt("IMultiSig", votingSystemContractAddress);
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
